026f65a2f6c1770dbc7264171744f85440bd0776
Sean D Gillespie <sean@mistersg.net>
2023-12-05 16:38:29 -0500

    chore: Add nix and cabal boilerplate

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        overlays = [haskellNix.overlay];
+
+        pkgs = import nixpkgs {
+          inherit system overlays; inherit (haskellNix) config;
+        };
+
+        project = pkgs.haskell-nix.project' {
+          src = ./.;
+          compiler-nix-name = "ghc963";
+
+          shell.tools = {
+            cabal = {};
+            fourmolu = {};
+            hlint = {};
+            haskell-language-server = {};
+          };
+
+          shell.buildInputs = with pkgs; [
+            nixpkgs-fmt
+          ];
+        };
+
+        flake = project.flake {};
+    in pkgs.lib.recursiveUpdate flake {
+      packages.default = flake.packages."hello:exe:hello";
+    });
+}
