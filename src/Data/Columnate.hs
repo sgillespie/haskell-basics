6e86cc14f3b2239ac5f8828aff2a365a4302d8e2
Sean D Gillespie <sean@mistersg.net>
2023-12-07 13:21:39 -0500

    chore: Add some lib boilerplate

---
+++ src/Data/Columnate.hs
@@ -0,0 +1,7 @@
+module Data.Columnate (columnate) where
+
+import Data.Text (Text ())
+import Data.Text qualified as Text
+
+columnate :: Text -> Text
+columnate _ = _
---
+++ src/Data/Columnate/Types.hs
@@ -0,0 +1,1 @@
+module Data.Columnate.Types () where
